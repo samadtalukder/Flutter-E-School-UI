5562fdbba82cb9d2157f94e060016f278590cd2d
samad talukder <samadtalukdar6@gmail.com>
2021-12-06 14:51:02 +0600

    app page route set

---
+++ lib/route/page_routes.dart
@@ -0,0 +1,11 @@
+class Routes{
+
+  static const home = '/';
+  static const splashScreen = '/Splash_Screen';
+  static const onBoarding = '/On_Boarding';
+  static const selectSchool = '/Select_School';
+  static const academicManagement = '/ Academic_Management';
+
+
+
+}
