a745b658c7ff0bc543588f32524acf40ca200139
samad talukder <samadtalukdar6@gmail.com>
2021-12-06 14:47:32 +0600

    app constants data added

---
+++ lib/app/app_constants.dart
@@ -0,0 +1,120 @@
+class Constants {
+  static const appName = "Flutter E-School UI";
+
+  static const slide_title_1 = "Total School Management Solution";
+  static const slide_content_1 =
+      "You can manage your whole school \n by this app";
+
+  static const slide_title_2 = "Attendance Management";
+  static const slide_title_content =
+      "You can manage your whole school \n by this app";
+
+  static const slide_title_3 = "Result & Exam Management";
+  static const slide_content_3 =
+      "You can manage your whole school \n by this app";
+
+  static const skip = "Skip";
+  static const next = "Next";
+  static const back = "Back";
+  static const enter = "Enter";
+  static const welcome = "Welcome!";
+  static const management = "Management";
+
+  static const school_name = "Dhaka College";
+
+  static const academic_management = "Academic Management";
+  static const admission_management = "Admission Management";
+  static const human_resources = "Human Resources";
+  static const accounts_management = "Accounts Management";
+  static const student_management = "Student Management";
+  static const routine_exam_management = "Routine & Exam Management";
+  static const result_management = "Result Management";
+  static const attendance_management = "Attendance Management";
+  static const leave_management = "Leave Management";
+
+  static const status = "Status";
+  static const phase = "Phase";
+  static const view = "View Details";
+  static const code = "Code";
+  static const subject_code = "Subject Code";
+  static const name = "Name";
+  static const label = "Label";
+  static const level = "Level";
+  static const semester_name = "Semester Name";
+  static const group_name = "Group Name";
+
+  static const departments_name = "Department Name";
+  static const class_list = "Class List";
+
+  static const inActive = "InActive";
+  static const active = "Active";
+
+  static const boards = "Boards";
+  static const about_institute = "About Institute";
+  static const session = "Session";
+  static const shifts = "Shift";
+  static const section = "Section";
+  static const semesters = "Semesters";
+  static const departments = "Departments";
+  static const groups = "Groups";
+  static const class_category = "Class Category";
+  static const classes = "Classes";
+  static const subjects = "Subjects";
+  static const exam_types = "Exam Types";
+  static const program_offer = "Program Offer";
+
+  static const from_date = "From Date";
+  static const to_date = "To Date";
+
+  static const exam_subject_and_mark = "Exam Subject & Mark";
+  static const class_period = "Class Period";
+  static const class_routine = "Class Routine";
+  static const exam_routine = "Exam Routine";
+  static const exam_date_and_time = "Exam Date & Time";
+  static const applicants = "Applicants";
+  static const applicant_seat_plan = "Applicant Seat Plan";
+  static const admission_seat_plan = "Admission Exam Seat Plan";
+  static const admission_result_position = "Admission Result Position";
+  static const building = "Building";
+  static const room = "Room";
+  static const exam_room = "Exam Room";
+
+  static const fee_info = "Fee Info";
+  static const all_fee_details = "All Fee Details";
+  static const all_transaction_info = "All Transaction Info";
+  static const class_wise_due = "Class Wise Due";
+  static const section_wise_due = "Section Wise Due";
+
+  static const employee_leave_by_categories = "Employee Leave By Categories";
+  static const leave_types = "Leave Types";
+  static const employee_leave_application_list =
+      "Employee Leave Application List";
+
+  static const time_setting = "Time Setting";
+  static const employee_daily_attendance = "Employee Daily Attendance";
+  static const date_wise_employee_attendance = "Date Wise Employee Attendance";
+  static const student_daily_attendance = "Student Daily Attendance";
+  static const date_wise_student_attendance = "Date Wise Student Attendance";
+
+  static const gpa = "GPA";
+  static const monthly_exam_name = "Monthly Exam Name";
+  static const seat_plan = "Seat Plan";
+  static const transcript = "Transcript";
+  static const tabulation = "Tabulation";
+
+  static const schoolUrlKey = 'schoolUrl';
+  static const schoolNameKey = 'schoolName';
+  static const introScreenKey = 'intro_screen';
+
+  static const loginStatusKey = 'logged_in';
+  static const loginUserType = 'user_type';
+  static const loginYesKey = 'login_yes';
+  static const loginNoKey = 'login_no';
+
+  static const super_admin = 'super-admin';
+  static const accounts = 'accounts';
+  static const student = 'student';
+  static const teacher = 'teacher';
+
+  static const no_access = 'You do not have the right to access';
+}
